946c6252c7f0c6eab07434be29068a8b86ccc2b3
Varadharajan Sethuraman <varadha.devops@gmail.com>
2017-08-14 16:12:51 -0500

    inputfilename path modified

--- python/nameSplitInFile.py
+++ python/nameSplitInFile.py
@@ -2,10 +2,9 @@ import os, os.path
 
 homedir = os.path.expanduser('~')
 INPUT_FILE = os.path.join(homedir, "data.txt")
-with open(INPUT_FILE) as f:
+with open("data.txt") as f:
     for line in f:
         #linedata = line.split(':')
         linedata = line.partition(":")
 
         print(linedata[0])
-
